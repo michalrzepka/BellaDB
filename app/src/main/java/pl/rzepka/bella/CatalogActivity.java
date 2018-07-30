488d5cb2776de0025833b1b93fa56f587f83ebfa
Michal Rzepka <micrzepka@gmail.com>
2018-07-30 19:40:52 +0200

    Added data read.

--- app/src/main/java/pl/rzepka/bella/CatalogActivity.java
+++ app/src/main/java/pl/rzepka/bella/CatalogActivity.java
@@ -6,7 +6,10 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.widget.TextView;
+
+import java.util.Random;
 
 import pl.rzepka.bella.data.BookDbHelper;
 import pl.rzepka.bella.data.BookContract.BookEntry;
@@ -30,11 +33,23 @@ public class CatalogActivity extends AppCompatActivity {
         queryData();
     }
 
+    /*
+    Touch screen to insert more dummy data
+     */
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        status = "";
+        insertData();
+        queryData();
+        return super.onTouchEvent(event);
+    }
+
     private void insertData() {
 
-        String productNameString = "name";
-        double priceDouble = 99.99;
-        int quantityInteger = 1;
+        String productNameString = "Timequake";
+        double priceDouble = 9.99;
+        int quantityInteger = new Random().nextInt((99 - 1) + 1) + 1;
         String supplierNameString = "ABC";
         String supplierPhoneString = "212-366-2000";
 
@@ -47,12 +62,12 @@ public class CatalogActivity extends AppCompatActivity {
         item.put(BookEntry.COLUMN_SUPPLIER_NAME, supplierNameString);
         item.put(BookEntry.COLUMN_SUPPLIER_PHONE, supplierPhoneString);
 
-        status += "Item parameters: " + item.toString() +"\n";
+        status += "Item parameters: " + item.toString() +"\n\n";
         statusTextView.setText(status);
 
         long newRowId = db.insert(BookEntry.TABLE_NAME, null, item);
 
-        status += "Item inserted at row: " + newRowId +"\n";
+        status += "Item inserted at row: " + newRowId +"\n\n";
         statusTextView.setText(status);
 
     }
@@ -62,14 +77,36 @@ public class CatalogActivity extends AppCompatActivity {
         SQLiteDatabase db = mDBHelper.getReadableDatabase();
 
         String[] projection = {BookEntry._ID, BookEntry.COLUMN_PRODUCT_NAME, BookEntry.COLUMN_SUPPLIER_NAME, BookEntry.COLUMN_QUANTITY};
-        String selection = BookEntry.COLUMN_SUPPLIER_NAME + "=?";
-        String[] selectionArgs = {"ABC"};
+        String selection = BookEntry.COLUMN_SUPPLIER_NAME + "=? AND " + BookEntry.COLUMN_QUANTITY + ">?";
+        String[] selectionArgs = {"ABC", "1"};
 
-        Cursor cursor = db.query(BookEntry.TABLE_NAME, projection, selection, selectionArgs, null, null, BookEntry._ID + " DESC");
+        Cursor cursor = db.query(BookEntry.TABLE_NAME, projection, selection, selectionArgs, null, null, BookEntry._ID + " DESC", null);
+
         try {
 
-            status += "Items in DB: " + cursor.getCount() + "\n";
+            status += "Items found DB: " + cursor.getCount() + "\n\n";
             statusTextView.setText(status);
+
+            cursor.moveToFirst();
+
+            int idColumnIndex = cursor.getColumnIndex(BookEntry._ID);
+            int nameColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_PRODUCT_NAME);
+            int supplierColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_SUPPLIER_NAME);
+            int quantityColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_QUANTITY);
+
+            int resultId = cursor.getInt(idColumnIndex);
+            String resultName = cursor.getString(nameColumnIndex);
+            String resultSupplier = cursor.getString(supplierColumnIndex);
+            int resultQuantity = cursor.getInt(quantityColumnIndex);
+
+            status += "First found item:\n" +
+                    "ID: " + resultId + "\n" +
+                    "Name: " + resultName + "\n" +
+                    "Supplier: " + resultSupplier + "\n" +
+                    "Quantity: " + resultQuantity + "\n";
+
+            statusTextView.setText(status);
+
         } finally {
             cursor.close();
         }
